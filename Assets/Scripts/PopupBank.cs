751899bca9836c0431ba0271909bd002928af097
ghdwlsdl1 <ghdwlsdl1@naver.com>
2025-06-09 10:53:13 +0900

    feat 7단계UI구현
    입출금 창 이동

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -8,20 +8,57 @@ public class PopupBank : MonoBehaviour
     public Text balanceText;
     public Text cashText;
 
+    [Header("팝업 창")]
+    public GameObject depositUI;
+    public GameObject withdrawalUI;
+
+    [Header("메인 버튼 그룹")]
+    public GameObject mainButtonsGroup;
+
+    [Header("버튼")]
+    public Button depositButton;
+    public Button withdrawButton;
+    public Button backFromDepositButton;
+    public Button backFromWithdrawButton;
+
     private void Start()
     {
         Refresh();
+        CloseAllPopup();
+
+        depositButton.onClick.AddListener(OpenDepositUI);
+        withdrawButton.onClick.AddListener(OpenWithdrawalUI);
+        backFromDepositButton.onClick.AddListener(CloseAllPopup);
+        backFromWithdrawButton.onClick.AddListener(CloseAllPopup);
     }
 
     public void Refresh()
     {
         var data = GameManager.Instance.userData;
 
-        // 유저 이름
         userNameText.text = data.userName;
-
-        // 금액 포맷팅 (천 단위 , 추가)
         balanceText.text = string.Format("{0:N0}", data.balance);
         cashText.text = string.Format("{0:N0}", data.cash);
+    }
+
+    public void OpenDepositUI()
+    {
+        depositUI.SetActive(true);
+        withdrawalUI.SetActive(false);
+        mainButtonsGroup.SetActive(false);
+    }
+
+    public void OpenWithdrawalUI()
+    {
+        withdrawalUI.SetActive(true);
+        depositUI.SetActive(false);
+        mainButtonsGroup.SetActive(false);
+    }
+
+    public void CloseAllPopup()
+    {
+        depositUI.SetActive(false);
+        withdrawalUI.SetActive(false);
+        mainButtonsGroup.SetActive(true);
     }
 }
